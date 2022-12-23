b101d0917c23e188667155bbe80e39ba59d8e3a1
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2022-12-23 16:02:24 +0300

    v0

--- webtutor_dag.py
+++ webtutor_dag.py
@@ -130,6 +130,15 @@ def transform(data, data_type):
         data['modificator_id'] = data['modificator_id'].fillna(0).astype(np.int64)
         data['id_stoyanki'] = data['id_stoyanki'].fillna(0).astype(np.int64)
         data['id_plowadki'] = data['id_plowadki'].fillna(0).astype(np.int64)
+    elif not data.empty and data_type == 'subdivisions':
+        data['org_id'] = data['org_id'].fillna(0).astype(np.int64)
+        data['parent_object_id'] = data['parent_object_id'].fillna(0).astype(np.int64)
+        data['place_id'] = data['place_id'].fillna(0).astype(np.int64)
+        data['cost_center_id'] = data['cost_center_id'].fillna(0).astype(np.int64)
+        data['app_instance_id'] = data['app_instance_id'].fillna(0).astype(np.int64)
+        data['region_id'] = data['region_id'].fillna(0).astype(np.int64)
+        data['kpi_profile_id'] = data['kpi_profile_id'].fillna(0).astype(np.int64)
+        data['bonus_profile_id'] = data['bonus_profile_id'].fillna(0).astype(np.int64)
     return data
 
 def load(data, data_type):
