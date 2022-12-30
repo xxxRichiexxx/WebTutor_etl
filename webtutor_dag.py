be93971f06be0d02887875bcbd2d98ef4e0b9713
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2022-12-30 09:36:15 +0300

    v2

--- webtutor_dag.py
+++ webtutor_dag.py
@@ -116,7 +116,7 @@ regions =  """
             """
 
 places =  """
-            SELECT id, code, name, modification_date, region_id, timezone_id
+            SELECT id, code, name, modification_date, region_id, timezone_id,
             COALESCE(region_id, 0) AS region_id,
             COALESCE(timezone_id, 0) AS timezone_id
             FROM {0}
