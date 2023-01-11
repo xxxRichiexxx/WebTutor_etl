d35305d8f534be4abd8ac52b0bf059175b69117d
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-01-11 12:36:54 +0300

    v.3

--- mart.sql
+++ mart.sql
@@ -27,7 +27,7 @@ WITH
 	FULL JOIN sq2
 		ON sq1.subdivision_id = sq2.subdivision_id AND sq1."Год трудоустройства" = sq2."Год увольнения"
 	),
-	-----"МП. Факт"--------
+		-----"МП. Факт"--------
 	sq4 AS(
 	SELECT
 		subdivision_id,
@@ -35,7 +35,6 @@ WITH
 		MAX("Прирост") OVER (PARTITION BY subdivision_id ORDER BY "Год") - 
 			MAX("Убывание") OVER (PARTITION BY subdivision_id ORDER BY "Год") AS "МП. Факт"
 	FROM sq3
-	ORDER BY subdivision_id DESC, "Год"
 	),
 	------------Динамика количества старших менеджеров по продажам--------------
 	sq5 AS(
@@ -65,7 +64,7 @@ WITH
 	FULL JOIN sq6
 		ON sq5.subdivision_id = sq6.subdivision_id AND sq5."Год трудоустройства" = sq6."Год увольнения"
 	),
-	-----"СтМОП. Факт"--------
+		-----"СтМОП. Факт"--------
 	sq8 AS(
 	SELECT
 		subdivision_id,
@@ -73,7 +72,6 @@ WITH
 		MAX("Прирост") OVER (PARTITION BY subdivision_id ORDER BY "Год") -
 			MAX("Убывание") OVER (PARTITION BY subdivision_id ORDER BY "Год") AS "СтМОП. Факт"
 	FROM sq7
-	ORDER BY subdivision_id DESC, "Год"
 	),
 	------------Динамика количества экспертов по корпоративным продажам--------------
 	sq9 AS(
@@ -103,7 +101,7 @@ WITH
 	FULL JOIN sq10
 		ON sq9.subdivision_id = sq10.subdivision_id AND sq9."Год трудоустройства" = sq10."Год увольнения"
 	),
-	-----"ЭКорП. Факт"--------
+		-----"ЭКорП. Факт"--------
 	sq12 AS(
 	SELECT
 		subdivision_id,
@@ -111,7 +109,6 @@ WITH
 		MAX("Прирост") OVER (PARTITION BY subdivision_id ORDER BY "Год") -
 			MAX("Убывание") OVER (PARTITION BY subdivision_id ORDER BY "Год") AS "ЭКорП. Факт"
 	FROM sq11
-	ORDER BY subdivision_id DESC, "Год"
 	),
 	------------Динамика количества руководителей отделов продаж--------------
 	sq13 AS(
@@ -141,7 +138,7 @@ WITH
 	FULL JOIN sq14
 		ON sq13.subdivision_id = sq14.subdivision_id AND sq13."Год трудоустройства" = sq14."Год увольнения"
 	),
-	-----"РОП. Факт"--------
+		-----"РОП. Факт"--------
 	sq16 AS(
 	SELECT
 		subdivision_id,
@@ -149,7 +146,6 @@ WITH
 		MAX("Прирост") OVER (PARTITION BY subdivision_id ORDER BY "Год") -
 			MAX("Убывание") OVER (PARTITION BY subdivision_id ORDER BY "Год") AS "РОП. Факт"
 	FROM sq15
-	ORDER BY subdivision_id DESC, "Год"
 	),
 	------------Динамика количества маркетологов--------------
 	sq17 AS(
@@ -179,7 +175,7 @@ WITH
 	FULL JOIN sq18
 		ON sq17.subdivision_id = sq18.subdivision_id AND sq17."Год трудоустройства" = sq18."Год увольнения"
 	),
-	-----"Маркетолог. Факт"--------
+		-----"Маркетолог. Факт"--------
 	sq20 AS(
 	SELECT
 		subdivision_id,
@@ -187,38 +183,83 @@ WITH
 		MAX("Прирост") OVER (PARTITION BY subdivision_id ORDER BY "Год") -
 			MAX("Убывание") OVER (PARTITION BY subdivision_id ORDER BY "Год") AS "Маркетолог. Факт"
 	FROM sq19
-	ORDER BY subdivision_id DESC, "Год"
+	),
+	----------------Диапазон дат (годов)-------------------------------
+	years AS(
+	SELECT EXTRACT(YEAR FROM NOW()) AS "Год"
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-1
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-2
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-3
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-4
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-5
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-6
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-7
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-8
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-9
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-10
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-11
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-12
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-13
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-14
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-15
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-16
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-17
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-18
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-19
+	UNION
+	SELECT EXTRACT(YEAR FROM NOW())-20
 	)
 SELECT
 	o.disp_name AS org_name,
 	s.id AS subdivision_id,
 	s.name AS sub_name,
 	p.plan_name,
+	years."Год",
 	p.plan_value,
 	s.type_dc,
 	st."Менеджер по продажам новых автомобилей (МП)" AS "Менеджер по продажам новых автомобилей (МП). План",
-	MAX(sq4."МП. Факт") OVER (PARTITION BY s.subdivision_id  ORDER BY p.plan_name) AS "МП. Факт",
+	LAST_VALUE(sq4."МП. Факт" ignore nulls) OVER (PARTITION BY s.subdivision_id  ORDER BY years."Год") AS "МП. Факт",
 	st."Cтарший менеджер по продажам (СтМОП)" AS "Cтарший менеджер по продажам (СтМОП). План",
-	MAX(sq8."СтМОП. Факт") OVER (PARTITION BY s.subdivision_id  ORDER BY p.plan_name) AS "СтМОП. Факт",
+	LAST_VALUE(sq8."СтМОП. Факт" ignore nulls) OVER (PARTITION BY s.subdivision_id  ORDER BY years."Год") AS "СтМОП. Факт",
 	st."Эксперт по корпоративным продажам (ЭКорП)" AS "Эксперт по корпоративным продажам (ЭКорП). План",
-	MAX(sq12."ЭКорП. Факт") OVER (PARTITION BY s.subdivision_id  ORDER BY p.plan_name) AS "ЭКорП. Факт",
+	LAST_VALUE(sq12."ЭКорП. Факт" ignore nulls) OVER (PARTITION BY s.subdivision_id  ORDER BY years."Год") AS "ЭКорП. Факт",
 	st."Руководитель отдела продаж (РОП)" AS "Руководитель отдела продаж (РОП). План",
-	MAX(sq16."РОП. Факт") OVER (PARTITION BY s.subdivision_id  ORDER BY p.plan_name) AS "РОП. Факт",
-	MAX(sq20."Маркетолог. Факт") OVER (PARTITION BY s.subdivision_id  ORDER BY p.plan_name) AS "Маркетолог. Факт"
+	LAST_VALUE(sq16."РОП. Факт" ignore nulls) OVER (PARTITION BY s.subdivision_id  ORDER BY years."Год") AS "РОП. Факт",
+	LAST_VALUE(sq20."Маркетолог. Факт" ignore nulls) OVER (PARTITION BY s.subdivision_id  ORDER BY years."Год") AS "Маркетолог. Факт"
 FROM sttgaz.dds_webtutor_subdivision AS s
-INNER JOIN sttgaz.dds_webtutor_plans AS p 
-	ON s.id = p.subdivision_id AND p.plan_name IS NOT NULL
+CROSS JOIN years
+LEFT JOIN sttgaz.dds_webtutor_plans AS p 
+	ON s.id = p.subdivision_id AND RIGHT(p.plan_name, 4)::INT = years."Год"
 LEFT JOIN sttgaz.dds_webtutor_standart AS st 
 	ON s.type_dc::varchar = st.type_dc::varchar
 LEFT JOIN sttgaz.dds_webtutor_orgs AS o 
 	ON s.org_id = o.id
 LEFT JOIN sq4
-	ON s.subdivision_id = sq4.subdivision_id AND RIGHT(p.plan_name, 4)::INT = sq4."Год" 
+	ON s.subdivision_id = sq4.subdivision_id AND years."Год" = sq4."Год" 
 LEFT JOIN sq8
-	ON s.subdivision_id = sq8.subdivision_id AND RIGHT(p.plan_name, 4)::INT = sq8."Год" 
+	ON s.subdivision_id = sq8.subdivision_id AND years."Год" = sq8."Год" 
 LEFT JOIN sq12
-	ON s.subdivision_id = sq12.subdivision_id AND RIGHT(p.plan_name, 4)::INT = sq12."Год" 
+	ON s.subdivision_id = sq12.subdivision_id AND years."Год" = sq12."Год" 
 LEFT JOIN sq16
-	ON s.subdivision_id = sq16.subdivision_id AND RIGHT(p.plan_name, 4)::INT = sq16."Год" 	
+	ON s.subdivision_id = sq16.subdivision_id AND years."Год" = sq16."Год" 
 LEFT JOIN sq20
-	ON s.subdivision_id = sq20.subdivision_id AND RIGHT(p.plan_name, 4)::INT = sq20."Год"
+	ON s.subdivision_id = sq20.subdivision_id AND years."Год" = sq20."Год" 
