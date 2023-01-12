0eeeb9a764b2ad7a666e759c04a1a42f4cecabaa
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-01-12 13:44:45 +0300

    v.3

--- stage_webtutor_position_commons.sql
+++ stage_webtutor_position_commons.sql
@@ -1,6 +1,6 @@
 SELECT
     id, code, name,
-    COALESCE(position_familys, 0)                 AS position_familys,
+    COALESCE(position_familys, '0')              AS position_familys,
     modification_date
 FROM {0}
 WHERE modification_date > CAST('{1}' AS DATETIME2);
