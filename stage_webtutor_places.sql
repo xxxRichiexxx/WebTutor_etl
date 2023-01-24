d992a52d91d1e2b9eb9fd9a912961a0f52de4205
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-01-24 15:04:12 +0300

    v.5

--- stage_webtutor_places.sql
+++ stage_webtutor_places.sql
@@ -2,6 +2,6 @@ SELECT
     id, code, name, modification_date,
     COALESCE(region_id, 0)                                      AS region_id,
     COALESCE(timezone_id, 0)                                    AS timezone_id,
-    COALESCE(parent_object_id, 0)                               AS parent_object_id
+    COALESCE(parent_id, 0)                                      AS parent_object_id
 FROM {0}
 WHERE modification_date > CAST('{1}' AS DATETIME2);
